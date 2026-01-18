1acce435ba09eab8147ad3d1e6f530223441b31e
Ralph Andy Menz <andyralphmenz@gmail.com>
2026-01-18 23:56:33 +0000

    ...

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -9,10 +9,10 @@ const Explore = () => {
                     <h2>
                         Explore more <span className="purple"> Books</span>
                     </h2>
-                    {/* <Link to="/books">
+                    <Link to="/books">
                         <button className='btn'>Explore books </button>
                     </Link>
-                </div> */}
+                </div>
             </div>
         </section>
     );
